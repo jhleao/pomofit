7e57e22dbaae9374a68dc428837917efbed26924
José Henrique Leão <leao@pop-os.localdomain>
2021-05-17 16:29:39 -0300

    fix fonts v2

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import Layout from '@components/Layout';
 import { ApiProvider } from '@contexts/ApiContext';
 import GlobalProvider from '@contexts/GlobalContext';
 import { ModalProvider } from '@contexts/ModalContext';
+import '../fonts.css';
 
 function MyApp({ Component, pageProps }) {
   return (
